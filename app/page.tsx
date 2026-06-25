41b171a7edbe3473145dd0cd5f74eba01d6b69b2
Sara Goldberger <sarale2003@gmail.com>
2026-06-25 09:50:41 -0700

    Added title metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 // STRUCTURE and INTERACTIVITY of a webpage
 
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface';
 import AdoptButton from './adopt-button';
+
+export const metadata: Metadata = {
+  title: 'Dog Shelter',
+};
 
 // Component that forms the homepage
 export default function Home() {
